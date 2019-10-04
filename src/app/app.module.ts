2b1026bf7add5de34bbf973733a9931ec140bc83
bmarti <31932714+bmarti@users.noreply.github.com>
2019-10-04 16:46:02 +0200

    Ajout: modification du nom du héros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     [RouterModule.forRoot(routes)],
     BrowserModule,
+    FormsModule,
   ],
   exports: [RouterModule],
   bootstrap: [AppComponent]
