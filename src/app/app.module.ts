57749da519d25ba4f395e6ecd15d124296e4ebb8
bmarti <31932714+bmarti@users.noreply.github.com>
2019-10-04 15:52:10 +0200

    ajout du premier héros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent}
+  {path: '', component: AppComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent
   ],
   imports: [
     [RouterModule.forRoot(routes)],
