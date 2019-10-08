d061c2552c4cd3471b57d0884ea0cc2044b70ce8
bmarti <31932714+bmarti@users.noreply.github.com>
2019-10-08 11:05:20 +0200

    Ajout: import module route + barre de nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [
-  {path: '', component: AppComponent},
-];
 
 @NgModule({
   declarations: [
@@ -21,11 +17,10 @@ const routes: Routes = [
     MessagesComponent
   ],
   imports: [
-    [RouterModule.forRoot(routes)],
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
   ],
-  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 
